59082587c6d6c3eee0f535f5535999a91732d95d
paulboco <paulboco@cableone.net>
2015-04-18 14:50:06 -0500

    Add files with insecure config.inc.php.

---
+++ config.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+$cfg['Servers'][1]['auth_type'] = 'config';
+$cfg['Servers'][1]['host'] = 'localhost'; //edit if you have db in the other host
+$cfg['Servers'][1]['connect_type'] = 'tcp';
+$cfg['Servers'][1]['compress'] = false;
+$cfg['Servers'][1]['extension'] = 'mysql';
+$cfg['Servers'][1]['user'] = 'homestead'; //edit this line
+$cfg['Servers'][1]['password'] = 'secret'; // edit this line
