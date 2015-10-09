7021280abd0494746d1ce25ef2d94afac6ad9f9f
Paulbo Co <paulboco@cableone.net>
2015-10-09 14:07:30 -0500

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -4,6 +4,6 @@ $cfg['blowfish_secret'] = 'za17c1ec07d61548';
 
 $i=0;
 $i++;
-$cfg['Servers'][$i]['user']      = 'homestead';
-$cfg['Servers'][$i]['password']  = 'secret';
+$cfg['Servers'][$i]['user']      = 'root';
+$cfg['Servers'][$i]['password']  = 'root';
 $cfg['Servers'][$i]['auth_type'] = 'config';
