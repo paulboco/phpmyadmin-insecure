d18cdce80afde4544d65117846b6b6fd5dd58880
paulboco <paulboco@cableone.net>
2016-02-16 22:30:04 -0600

    Bypass secure login when in 'local' environment.

--- config.inc.php
+++ config.inc.php
@@ -1,15 +1,14 @@
 <?php
 
-// Set to true for secure login
-$__SECURE = isset($_SERVER['APP_ENV']) && $_SERVER['APP_ENV'] == 'production';
-
+// Set security key.
 $cfg['blowfish_secret'] = '54E87DC55GDSF5SDDsd545487';
 
-if ($__SECURE) {
-    $cfg['Servers'][1]['auth_type'] = 'cookie';
-    $cfg['Servers'][1]['AllowNoPassword'] = false;
-} else {
+// Only the 'local' environment may login insecurely.
+if (getenv('APP_ENV') == 'local') {
     $cfg['Servers'][1]['auth_type'] = 'config';
     $cfg['Servers'][1]['user']      = 'homestead';
     $cfg['Servers'][1]['password']  = 'secret';
+} else {
+    $cfg['Servers'][1]['auth_type'] = 'cookie';
+    $cfg['Servers'][1]['AllowNoPassword'] = false;
 }
