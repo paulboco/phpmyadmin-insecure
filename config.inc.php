cfcd08ab520d5f4f355403e36fc8ca8a84118305
paulboco <paulboco@cableone.net>
2016-02-16 19:09:28 -0600

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -1,18 +1,14 @@
 <?php
 
 $__SECURE = false;
+
 $cfg['blowfish_secret'] = '54E87DC55GDSF5SDDsd545487';
 
-$i = 0;
-$i++;
-
 if ($__SECURE) {
-    $cfg['Servers'][$i]['auth_type'] = 'cookie';
-    $cfg['Servers'][$i]['AllowNoPassword'] = false;
-}
-else
-{
-    $cfg['Servers'][$i]['auth_type'] = 'config';
-    $cfg['Servers'][$i]['user']      = 'homestead';
-    $cfg['Servers'][$i]['password']  = 'secret';
+    $cfg['Servers'][1]['auth_type'] = 'cookie';
+    $cfg['Servers'][1]['AllowNoPassword'] = false;
+} else {
+    $cfg['Servers'][1]['auth_type'] = 'config';
+    $cfg['Servers'][1]['user']      = 'homestead';
+    $cfg['Servers'][1]['password']  = 'secret';
 }
